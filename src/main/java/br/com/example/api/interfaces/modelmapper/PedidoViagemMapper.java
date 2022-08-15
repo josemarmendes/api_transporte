396263c24c2bf6cbab7487740b0480eea4c877d0
josemar mendes <josemarmendes17@gmail.com>
2022-08-15 17:11:19 -0300

    adicionando hateoas uso de link em subrecurso de pedido de viagens

--- src/main/java/br/com/example/api/interfaces/modelmapper/PedidoViagemMapper.java
+++ src/main/java/br/com/example/api/interfaces/modelmapper/PedidoViagemMapper.java
@@ -4,8 +4,14 @@ import br.com.example.api.domain.Passageiro;
 import br.com.example.api.domain.PassageiroRepository;
 import br.com.example.api.domain.PedidoViagem;
 import br.com.example.api.domain.PedidoViagemRepository;
+import br.com.example.api.interfaces.PassageiroAPI;
 import br.com.example.api.interfaces.input.PedidoViagemInput;
+import br.com.example.api.interfaces.output.PedidoViagemOutput;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -24,5 +30,30 @@ public class PedidoViagemMapper {
         pedidoViagem.setDestino(input.getDestino());
 
         return pedidoViagem;
+    }
+
+    public PedidoViagemOutput map(PedidoViagem pedidoViagem){
+        PedidoViagemOutput output = new PedidoViagemOutput();
+        output.setId(pedidoViagem.getId());
+        output.setOrigem(pedidoViagem.getOrigem());
+        output.setDestino(pedidoViagem.getDestino());
+        output.setStatus(pedidoViagem.getStatus());
+        output.setDataCriacao(pedidoViagem.getDataCriacao());
+
+        return output;
+    }
+
+    public EntityModel<PedidoViagemOutput> construirObjetoDeSaida(PedidoViagem pedidoViagem, PedidoViagemOutput output){
+        EntityModel<PedidoViagemOutput> model = EntityModel.of(output);
+
+        Link passageiroLink = WebMvcLinkBuilder
+                .linkTo(PassageiroAPI.class)
+                .slash(pedidoViagem.getPassageiro().getId())
+                .withRel("passageiro")
+                .withTitle(pedidoViagem.getPassageiro().getNome());
+
+        model.add(passageiroLink);
+
+        return model;
     }
 }
--- src/main/java/br/com/example/api/interfaces/output/PedidoViagemOutput.java
+++ src/main/java/br/com/example/api/interfaces/output/PedidoViagemOutput.java
@@ -3,21 +3,16 @@ package br.com.example.api.interfaces.output;
 import br.com.example.api.domain.Passageiro;
 import br.com.example.api.domain.PedidoViagemStatus;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
-@Entity
-public class PedidoViagemOutput {
-    @Id
-    @GeneratedValue
+public class PedidoViagemOutput{
     private Long id;
-    @ManyToOne
-    private Passageiro passageiro;
     private String origem;
     private String destino;
-    @Enumerated(EnumType.STRING)
     private PedidoViagemStatus status;
     private LocalDate dataCriacao;
 }
